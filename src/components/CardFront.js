dbef50c342a6f2a1735ebbbfb384c8ac5783c56c
spencerwrenfro <developer@spencerwrenfro.com>
2023-12-17 17:29:54 -0800

    adds responsive layout for extra small devices front header

--- src/components/CardFront.js
+++ src/components/CardFront.js
@@ -20,28 +20,28 @@ function CardBack() {
   const latin =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porttitor fermentum ante. Suspendisse ....";
   return (
-    <div className="mx-auto px-20 cardBackground lg:rounded-lg pb-20">
-      <div className="grid grid-cols-12 gap-6 py-10 items-center">
-        <div className="col-start-2 col-span-3 lg:col-span-2 flex items-center">
+    <div className="mx-auto lg:px-20 cardBackground lg:rounded-lg pb-20">
+      <div className="grid grid-cols-12 gap-3 md:gap-6 py-10 items-center">
+        <div className="col-span-12 md:col-start-2 md:col-span-2 flex items-center">
           <img
             //   src="https://gravatar.com/avatar/645ad8dc88b181886f74644b9866f080"
             src={profile}
-            className="w-full lg:w-3/4 mx-auto"
+            className="w-1/4 md:w-3/4 mx-auto"
             style={{ borderRadius: "50%", border: "solid 3px white" }}
             alt="headshot"
           />
         </div>
-        <div className="col-span-8 lg:col-span-9 flex flex-col lg:flex-row gap-3 lg:gap-0">
-          <div class="flex flex-col basis-1/2">
-            <h1 className="text-2xl lg:text-3xl font-bold text-salmon">Jane Smith</h1>
-            <h2 className="text-2xl lg:text-3xl text-salmon">Floral Designer</h2>
+        <div className="col-span-12 md:col-span-8 lg:col-span-8 flex flex-col lg:flex-row gap-6 md:gap-3 lg:gap-0">
+          <div class="flex flex-col basis-1/2 lg:basis-2/5 text-center md:text-left">
+            <h1 className="text-4xl md:text-2xl lg:text-3xl font-bold text-salmon">Jane Smith</h1>
+            <h2 className="text-4xl md:text-2xl lg:text-3xl text-salmon">Floral Designer</h2>
           </div>
-          <div className="flex flex-row items-center justify-start lg:justify-end gap-4 lg:gap-8 basis-1/2">
-            <img className="rounded-lg p-1 h-8 lg:h-10 w-8 lg:w-10 hover cursor-" src={instagram} alt="icon" />
-            <img className="rounded-lg p-1 h-8 lg:h-10 w-8 lg:w-10 hover cursor-pointer" src={facebook} alt="icon" />
-            <img className="rounded-lg p-1 h-8 lg:h-10 w-8 lg:w-10 hover cursor-pointer" src={pinterest} alt="icon" />
-            <img className="rounded-lg p-1 h-8 lg:h-10 w-8 lg:w-10 hover cursor-pointer" src={deviant} alt="icon" />
-            <img className="rounded-lg p-1 h-8 lg:h-10 w-8 lg:w-10 hover cursor-pointer" src={behance} alt="icon" />
+          <div className="flex flex-row items-center justify-center md:justify-start lg:justify-end gap-4 lg:gap-4 basis-1/2 lg:basis-3/5">
+            <img className="rounded-lg p-1 h-10 md:h-8 lg:h-10 w-10 md:w-8 lg:w-10 hover cursor-" src={instagram} alt="icon" />
+            <img className="rounded-lg p-1 h-10 md:h-8 lg:h-10 w-10 md:w-8 lg:w-10 hover cursor-pointer" src={facebook} alt="icon" />
+            <img className="rounded-lg p-1 h-10 md:h-8 lg:h-10 w-10 md:w-8 lg:w-10 hover cursor-pointer" src={pinterest} alt="icon" />
+            <img className="rounded-lg p-1 h-10 md:h-8 lg:h-10 w-10 md:w-8 lg:w-10 hover cursor-pointer" src={deviant} alt="icon" />
+            <img className="rounded-lg p-1 h-10 md:h-8 lg:h-10 w-10 md:w-8 lg:w-10 hover cursor-pointer" src={behance} alt="icon" />
           </div>
         </div>
       </div>
